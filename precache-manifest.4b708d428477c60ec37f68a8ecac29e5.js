35baa3fefefffc66957e4486e6f45516f0e023bf
Roman <romikkus@gmail.com>
2020-09-04 10:40:23 +0300

    Updates

---
+++ precache-manifest.4b708d428477c60ec37f68a8ecac29e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4826bb193083998ecf0683df4db8c830",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2f4c8bd7c549eb2773d",
+    "url": "./static/css/main.ac9457a2.chunk.css"
+  },
+  {
+    "revision": "561717c9b4db08d1f689",
+    "url": "./static/js/2.7b08be7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7b08be7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f4c8bd7c549eb2773d",
+    "url": "./static/js/main.9e846d35.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
